4661742cac6aa1fb2382a4ddd8ac0646826dace5
daspony <sebastian.vallejos@uc.cl>
2025-05-03 22:04:06 -0400

    Test navbar, load file

--- src/callbacks/__init__.py
+++ src/callbacks/__init__.py
@@ -4,7 +4,6 @@ from .upload_callbacks import register_upload_callbacks
 def register_callbacks(app):
 
     #Layout callback
-    register_layout_callbacks(app)
 
     #Upload files callback
     register_upload_callbacks(app)
